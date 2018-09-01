b8ce4a789586ec392b6cf31e8b2668f3dfa5aa97
Samuel Abächerli <sarner@gmx.ch>
2018-09-01 17:41:03 +0200

    First basic implementation of Material  UI

---
+++ src/Menu.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import CompareIcon from '@material-ui/icons/Compare';
+import HomeIcon from '@material-ui/icons/Home';
+
+class Menu extends Component {
+    
+    render() {
+
+        return (
+            <div className="Menu">
+                <Drawer
+                    variant="permanent"
+                    anchor="right"
+                >
+                    <List>
+                        <ListItem button component={Link} to="/">
+                            <ListItemIcon>
+                                <HomeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Home" />
+                        </ListItem>
+                        <ListItem button component={Link} to="/budget">
+                            <ListItemIcon>
+                                <CompareIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Budget" />
+                        </ListItem>
+                    </List>
+                </Drawer>
+            </div>
+        );
+    }
+
+}
+
+export default Menu;
