eeb330c207495c7c57e372bed8190289249b22c4
Samuel Abächerli <sarner@gmx.ch>
2018-10-03 09:11:30 +0200

    Fix test

--- src/container/AppContainer.test.jsx
+++ src/container/AppContainer.test.jsx
@@ -5,10 +5,10 @@ import App from './AppContainer';
 import createStore from '../redux/store';
 
 const initialState = {};
-this.store = createStore(initialState);
+const store = createStore(initialState);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={this.store}><App /></Provider>, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
