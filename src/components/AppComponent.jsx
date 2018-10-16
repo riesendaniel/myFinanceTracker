b4b562cb7fd13f0b08c5a7a1baa8266f8b3c9a26
Daniel Riesen (C727636) <daniel.riesen@axa-winterthur.ch>
2018-10-16 09:25:36 +0200

    implementation message component

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -17,6 +17,7 @@ import Menu from './MenuComponent';
 import NotFound from "./NotFound";
 import NewOutgoingComponent from "./NewOutgoingComponent";
 import OutgoingListComponent from "./OutgoingListComponent";
+import Notifier from "./Notifier";
 
 const menuWidth = '300px';
 
@@ -36,6 +37,7 @@ const AppComponent = (props) => {
           <header>
             <Header />
             { menuState === 'open' && <Menu width={menuWidth} /> }
+            <Notifier />
           </header>
           <div className={classes.toolbarPlaceholder} />
           <main className={menuState === 'open' ? classes.main : undefined}>
