76b19701fb288c828f0c4ea03c587c9e69b7d849
Samuel Abächerli <sarner@gmx.ch>
2018-10-28 14:31:57 +0100

    Add tests for budget component

--- src/tests/BudgetComponent.test.jsx
+++ src/tests/BudgetComponent.test.jsx
@@ -1,16 +1,85 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import createStore from '../redux/store';
-import Budget from '../components/BudgetComponent';
+import configureStore from 'redux-mock-store';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { BottomNavigation } from '@material-ui/core';
+import { BudgetComponent } from '../components/BudgetComponent';
+import BudgetList from '../components/BudgetListComponent';
+import BudgetSummary from '../components/BudgetSummaryComponent';
 
-const initialState = {};
-const store = createStore(initialState);
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('BudgetComponent', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Provider store={store}><Budget /></Provider>, div);
-    ReactDOM.unmountComponentAtNode(div);
+  let wrapper;
+
+  beforeEach(() => {
+    const initialState = {
+      isLoading: false,
+      categories: [],
+      budget: [],
+    };
+    const props = {
+      doLoadBudget: jest.fn(),
+      doLoadMainCategories: jest.fn(),
+      history: { push: jest.fn() },
+      isLoading: false,
+      mainCategories: [
+        {
+          id: 2,
+          description: 'Haushalt',
+          color: '#FF9900',
+        },
+        {
+          id: 5,
+          description: 'Reisen',
+          color: '#00FF00',
+        },
+      ],
+      budget: [
+        {
+          id: 1,
+          mainCategoryId: 2,
+          category: 'Unterhalt',
+          period: 'monthly',
+          monthly: 100,
+          yearly: 1200,
+        },
+        {
+          id: 2,
+          mainCategoryId: 2,
+          category: 'Essen & Getränke',
+          period: 'monthly',
+          monthly: 250,
+          yearly: 3000,
+        },
+        {
+          id: 3,
+          mainCategoryId: 5,
+          category: 'öffentlicher Verkehr',
+          period: 'yearly',
+          monthly: 200,
+          yearly: 2400,
+        },
+      ],
+    };
+    const mockStore = configureStore();
+    const store = mockStore(initialState);
+    wrapper = shallow(<BudgetComponent store={store} {...props} />);
+  });
+
+  it('should render the plain component', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should render the budget list component', () => {
+    expect(wrapper.find(BudgetList)).toHaveLength(2);
+  });
+
+  it('should render the budget summary', () => {
+    expect(wrapper.find(BudgetSummary).exists()).toBe(true);
+  });
+
+  it('should render the bottom navigation', () => {
+    expect(wrapper.find(BottomNavigation).exists()).toBe(true);
   });
 });
