c08ba0b1de9bad4b08977eac8bc2b7d1bef71970
Samuel Abächerli <sarner@gmx.ch>
2018-11-18 15:41:11 +0100

    Spacing improvements

--- src/components/IncomeComponent.jsx
+++ src/components/IncomeComponent.jsx
@@ -70,13 +70,13 @@ class IncomeComponent extends Component {
               <Card>
                 <CardContent>
                   <Grid container direction="row-reverse" justify="flex-end">
-                    <Grid item xs={12} md={4} className={classes.grossPay}>
+                    <Grid item xs={12} md={4} className={isWidthDown('sm', width) ? classes.grossPay : undefined}>
                       <IncomeGrossPay />
                     </Grid>
                     <Grid item xs={12} md={8} className={isWidthUp('sm', width, false) ? classes.deductions : undefined}>
                       <IncomeDeductions />
                     </Grid>
-                    <Grid item xs={12} md={4} container justify="space-between" alignItems="center" className={classes.netPay}>
+                    <Grid item xs={12} md={4} container justify="space-between" alignItems="center" className={isWidthDown('sm', width) ? classes.netPay : undefined}>
                       <Typography color={isWidthDown('sm', width) ? 'textSecondary' : undefined}>Nettoeinkommen</Typography>
                       <Typography>{`${Math.round(netPay)} ${currency}`}</Typography>
                     </Grid>
