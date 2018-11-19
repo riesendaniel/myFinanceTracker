38eb1bc4f586e25f6c27f66c9b2f997f63416959
Samuel Abächerli <sarner@gmx.ch>
2018-11-19 18:18:49 +0100

    Improve not found page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  Card, CardContent,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 
-class NotFound extends Component {
-  render() {
-    return (
-      <div className="Header">
-        <h2>Not Found</h2>
-      </div>
-    );
-  }
-}
+const NotFound = () => (
+  <Grid container spacing={16} justify="center">
+    <Grid item xs={12} md={10}>
+      <Typography variant="headline" component="h2">Seite nicht gefunden</Typography>
+    </Grid>
+    <Grid item xs={12} md={10}>
+      <Card>
+        <CardContent>
+          <Typography>Die gewünschte Seite wurde leider nicht gefunden...</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  </Grid>
+);
 
 export default NotFound;
