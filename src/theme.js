5032939116d2542d12be38c86ab949e0e1b4f774
Samuel Abächerli <sarner@gmx.ch>
2018-11-20 06:45:13 +0100

    Responsive button add positioning

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core';
 const primaryColor = '#F9AA33';
 const secondaryColor = '#344955';
 
-const theme = {
+const theme = createMuiTheme({
   palette: {
     background: '#E5E5E5',
     primary: {
@@ -15,63 +15,70 @@ const theme = {
       contrastText: '#FFFFFF',
     },
   },
-  overrides: {
-    MuiButton: {
-      fab: {
+});
+
+theme.overrides = {
+  MuiButton: {
+    fab: {
+      position: 'absolute',
+      bottom: '32px',
+      right: '32px',
+      [theme.breakpoints.up('lg')]: {
         position: 'absolute',
         bottom: '32px',
-        right: '32px',
+        right: '332px',
       },
     },
-    MuiCard: {
-      root: {
-        backgroundColor: '#FFFFFF',
+  },
+  MuiCard: {
+    root: {
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  MuiDialog: {
+    paper: {
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  MuiMenu: {
+    paper: {
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  MuiTypography: {
+    headline: {
+      fontWeight: 500,
+    },
+  },
+  MuiListItem: {
+    root: {
+      '&$selected': {
+        backgroundColor: primaryColor,
       },
     },
-    MuiDialog: {
-      paper: {
-        backgroundColor: '#FFFFFF',
-      },
-    },
-    MuiMenu: {
-      paper: {
-        backgroundColor: '#FFFFFF',
-      },
-    },
-    MuiTypography: {
-      headline: {
-        fontWeight: 500,
-      },
-    },
-    MuiListItem: {
-      root: {
-        '&$selected': {
-          backgroundColor: primaryColor,
-        },
-      },
-    },
-    MuiTableRow: {
-      root: {
-        borderBottom: 'none',
-        '&:nth-of-type(2n+1)': {
-          '&:not($head)': {
-            '&:not($footer)': {
-              backgroundColor: 'rgba(229, 229, 229, 0.5)',
-            },
+  },
+  MuiTableRow: {
+    root: {
+      borderBottom: 'none',
+      '&:nth-of-type(2n+1)': {
+        '&:not($head)': {
+          '&:not($footer)': {
+            backgroundColor: 'rgba(229, 229, 229, 0.5)',
           },
         },
       },
     },
-    MuiInput: {
-      inputType: {
-        textAlign: 'right',
-      },
-      /* breakpoint einfügen */
-      input: {
+  },
+  MuiInput: {
+    inputType: {
+      textAlign: 'right',
+    },
+    input: {
+      [theme.breakpoints.down('xs')]: {
         textAlign: 'right',
       },
     },
   },
 };
 
-export default createMuiTheme(theme);
+export default theme;
