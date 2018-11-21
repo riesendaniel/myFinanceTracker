0b78f6db4eeb2563313615e158156971ea931f5b
Samuel Abächerli <sarner@gmx.ch>
2018-11-21 10:38:09 +0100

    Card is already included in StyledFirebaseAuth

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import {
-  Card, CardContent,
   Grid,
   Typography,
 } from '@material-ui/core';
@@ -13,11 +12,7 @@ const SignIn = () => (
       <Typography variant="headline" component="h2">Login</Typography>
     </Grid>
     <Grid item xs={12} md={10}>
-      <Card>
-        <CardContent>
-          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-        </CardContent>
-      </Card>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
     </Grid>
   </Grid>
 );
