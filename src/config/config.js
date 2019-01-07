d4c1971714f961dc9fcf6c5106825ea5bd9e5f5a
Samuel Abächerli <sarner@gmx.ch>
2019-01-07 17:50:59 +0100

    Convert environment variables to string

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@
 // Config for firebase
 // ------------------------------------
 export const firebaseConfig = {
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  authDomain: String(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+  databaseURL: String(process.env.REACT_APP_FIREBASE_DATABASE_URL),
+  apiKey: String(process.env.REACT_APP_FIREBASE_API_KEY),
+  projectId: String(process.env.REACT_APP_FIREBASE_PROJECT_ID),
 };
 
 export default firebaseConfig;
