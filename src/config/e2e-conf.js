bf5dd5a07ba085dd48f7aa084b5aeb7decb24b8c
Daniel Riesen <noreply@riesen.ch>
2019-01-11 12:32:07 +0100

    fix api key for e2e-test

--- src/config/e2e-conf.js
+++ src/config/e2e-conf.js
@@ -1,14 +1,13 @@
+// e2e-conf.js
 require('dotenv-flow').config();
-// conf.js
 
 exports.config = {
   sauceUser: String(process.env.SAUCE_USERNAME),
   sauceKey: String(process.env.SAUCE_ACCESS_KEY),
-  specs: ['e2e-tests/*test.js'],
+  specs: ['../e2e-tests/*test.js'],
 
   onPrepare: function () {
     browser.ignoreSynchronization = true;
-    var caps = browser.getCapabilities()
   },
 
   multiCapabilities: [{
@@ -21,12 +20,4 @@ exports.config = {
     maxInstances: 5
   }],
 
-  onComplete: function () {
-    var printSessionId = function (jobName) {
-      browser.getSession().then(function (session) {
-        console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-      });
-    }
-    printSessionId("Insert Job Name Here");
-  }
 }
