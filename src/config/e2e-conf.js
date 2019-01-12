dba6f476415b356b62677b39b90a6b312a976484
Samuel Abächerli <sarner@gmx.ch>
2019-01-12 09:16:31 +0100

    Fix linting errors

--- src/config/e2e-conf.js
+++ src/config/e2e-conf.js
@@ -1,4 +1,4 @@
-// e2e-conf.js
+/* eslint-disable no-undef */
 require('dotenv-flow').config();
 
 exports.config = {
@@ -6,7 +6,7 @@ exports.config = {
   sauceKey: String(process.env.SAUCE_ACCESS_KEY),
   specs: ['../e2e-tests/*test.js'],
 
-  onPrepare: function () {
+  onPrepare: () => {
     browser.ignoreSynchronization = true;
   },
 
@@ -15,9 +15,8 @@ exports.config = {
     screenResolution: '1280x1024',
     version: '71',
     platform: 'Windows 10',
-    name: "chrome-tests",
+    name: 'chrome-tests',
     shardTestFiles: true,
-    maxInstances: 5
+    maxInstances: 5,
   }],
-
-}
+};
